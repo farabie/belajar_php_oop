f1e0ab054b4056b1cca19b2e20c79e15d7a074d6
Muhammad Farabie <farabie024@gmail.com>
2023-11-03 05:27:06 +0700

    Inheritance Interface

--- Interface.php
+++ Interface.php
@@ -7,7 +7,10 @@ use Data\{Suzuki};
 
 $suzuki = new Suzuki();
 $suzuki->drive();
+// $suzuki->getBrand();
 
+
+echo "Nama Brand mobilnya adalah {$suzuki->getBrand()}" . PHP_EOL;
 echo "Suzuki mempunyai roda {$suzuki->getTired()}" . PHP_EOL;
 
 ?>
--- data/Car.php
+++ data/Car.php
@@ -3,15 +3,36 @@
 namespace Data;
 
 //Secara otomatis interface itu class dan functionnya abstract
-interface Car
+
+interface hasBrand
+{
+    function getBrand(): string;
+}
+
+interface isMaintence
+{
+    function isMaintance(): bool;
+}
+
+//Contoh inheritance untuk interface
+interface Car extends hasBrand
 {
     function drive(): void;
 
     function getTired(): int;
 }
 
-class Suzuki implements Car
+class Suzuki implements Car, isMaintence
 {
+    public function getBrand(): string {
+        return "Suzuki";
+    }
+
+    public function isMaintance():bool 
+    {
+        return false;
+    }
+
     public function drive(): void
     {
         echo "Suzuki melaju" . PHP_EOL;
