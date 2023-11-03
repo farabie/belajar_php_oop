d71b2b7972e98f298bbf83776ada2ea888612f34
Muhammad Farabie <farabie024@gmail.com>
2023-11-03 17:50:25 +0700

    Iterator

--- ObjectIteration.php
+++ ObjectIteration.php
@@ -1,11 +1,23 @@
 <?php 
 
-class Data{
+class Data implements IteratorAggregate{
     var string $first = "First";
     public string $second = "Second";
     private string $third = "Third";
 
     protected string $fourth = "Forth";
+
+    //Kalau menggunakan iterator kita bisa menggambil datanya yang private sekalipun
+    //Tetap kita harus mengimplement juga untuk fungsi getIteratornya
+    public function getIterator() {
+        $array = [
+            "first" => $this->first,
+            "second"=> $this->second,
+            "third"=> $this->third,
+            "forth"=> $this->fourth,
+        ];
+        return new ArrayIterator($array);
+    }
 }
 
 $data = new Data();
