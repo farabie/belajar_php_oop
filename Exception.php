ed10449c601e5e443d160249554a37b866238083
Muhammad Farabie <farabie024@gmail.com>
2023-11-04 15:39:48 +0700

    Debug Exception

--- Exception.php
+++ Exception.php
@@ -7,15 +7,23 @@ require_once "helper/ValidationRequest.php";
 require_once "exception/ValidationException.php";
 
 $loginRequest = new LoginRequest();
-$loginRequest->username = "    ";
-$loginRequest->password = "    ";
+$loginRequest->username = "   ";
+$loginRequest->password = "   ";
 
 
 try {
     validateLoginRequest($loginRequest);
     echo "Valid" . PHP_EOL;
 } catch (ValidationException | Exception $exception) {
+
+    //Sebagai string
     echo "Error : {$exception->getMessage()}" . PHP_EOL;
+
+    var_dump($exception->getTrace());
+}
+// Block finally adalah block dimana akan selalu dieksekusi baik itu exception ataupun tidak
+finally {
+    echo "Error Tidak error tetap dipanggil" . PHP_EOL;
 }
 
 // catch (Exception $exception) {
