4a5c018ffa6cb178f682d612aa590b0bad52ca30
Muhammad Farabie <farabie024@gmail.com>
2023-11-04 15:56:42 +0700

    Reqular expression split

--- RegularExpression.php
+++ RegularExpression.php
@@ -2,10 +2,20 @@
 
 $matches = [];
 //Kita akan mencari dengan kata2 dibawah ini untuk yang /i itu fungsinya tidak peduli di case sensitif
+//$pattern $subject
+//Kalau tidak pakai bool maka yang di matches nya itu int atau jumlah yang matchnya
 $result = (bool)preg_match_all("/far|bie|muh/i", "Muhammad Farabie", $matches);
 
 var_dump($result);
 var_dump($matches);
+
+//$pattern, $replacement(diganti apa), $subject
+$sensor = preg_replace("/anjing|bangsat/i", "***", "dasar lu anjing, bangsat");
+var_dump($sensor);
+
+//Split
+$split = preg_split("/[\s,-]/", "Muhammad Farabie, Belajar-Coding");
+var_dump($split);
 
 
 
